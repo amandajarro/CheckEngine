6352d165df61b5e072169d34cf2396180d0a33ca
Tyler Whitehouse <twhitehouse@hawkmail.hccfl.edu>
2020-02-23 22:26:18 -0500

    added base window with button
    + main window file

--- CheckEngineProject/src/hcc/checkengine/search/CheckEngine.java
+++ CheckEngineProject/src/hcc/checkengine/search/CheckEngine.java
@@ -1,10 +1,24 @@
 package hcc.checkengine.search;
+import javax.swing.*;
+/**
+ * 
+ * @author Tyler Whitehouse
+ *
+ */
 
-public class CheckEngine {
-
-	public static void main(String[] args) {
-		
-
-	}
-
+public class CheckEngine
+{
+    public static void main(String[] args) 
+    {
+        
+        // makes Swing data thread safe
+        SwingUtilities.invokeLater(new Runnable()
+            {
+                public void run()
+                {
+                	new MainWindow();
+                }
+            });
+    }
 }
+
---
+++ CheckEngineProject/src/hcc/checkengine/search/MainWindow.java
@@ -0,0 +1,27 @@
+package hcc.checkengine.search;
+import java.awt.*;
+import javax.swing.*;
+
+public class MainWindow extends JFrame 
+{
+	private JTextArea textArea;
+	private JButton btn;
+
+	public MainWindow()
+	{
+		super ("Main Window");
+		
+		setLayout(new BorderLayout());
+		
+		textArea = new JTextArea();
+		btn = new JButton("button test");
+
+		add(textArea, BorderLayout.CENTER);
+		add(btn, BorderLayout.NORTH);
+
+		setSize(600, 500);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+}
