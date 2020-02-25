1788c7bc928245c04718b3747f997c60ab78dad9
Tyler Whitehouse <twhitehouse@hawkmail.hccfl.edu>
2020-02-25 04:37:01 -0500

    added functionality to about button

--- CheckEngineProject/src/hcc/checkengine/search/MainWindow.java
+++ CheckEngineProject/src/hcc/checkengine/search/MainWindow.java
@@ -1,23 +1,43 @@
 package hcc.checkengine.search;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MainWindow extends JFrame 
 {
 	private JTextArea textArea;
-	private JButton btn;
-
+	private JButton abtBtn;
+	
 	public MainWindow()
 	{
 		super ("Search Engine");
 		
-		setLayout(new BorderLayout());
+//		setLayout(new BorderLayout());
 		
-		textArea = new JTextArea();
-		btn = new JButton("button test");
-
-		add(textArea, BorderLayout.CENTER);
-		add(btn, BorderLayout.NORTH);
+		textArea = new JTextArea("search");
+		
+		// abtBtn holds information about the program
+		abtBtn = new JButton("About");
+		abtBtn.setBounds(50,50,50,50);
+		
+		
+		abtBtn.addActionListener(new ActionListener ()
+			{
+				public void actionPerformed(ActionEvent argO) 
+				{
+					
+					JFrame about = new JFrame("About");    
+					about.setSize(400,400);  
+					about.setLayout(null);  
+					about.setVisible(true);
+					about.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				}
+			});
+		
+//		add(textArea, BorderLayout.NORTH);
+		add(abtBtn);
 
 		setSize(600, 500);
         setVisible(true);
