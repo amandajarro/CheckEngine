1d3d2a22f7dbf0d26a96b5ffd65652d9b45ef479
amandajarro <ajaroszewicz@hawkmail.hccfl.edu>
2020-03-10 19:25:48 -0400

    Added a Main Class Added a manageIndex Class with prompts to create methods within.

---
+++ CheckEngineProject/src/GUI/main.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CheckEngineProject/src/GUI/manageIndex.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class manageIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
